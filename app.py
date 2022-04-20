78cd846d571e3d5a1a48a87689035fb6d464834c
Toby Freeland <tobyfreeland@gmail.com>
2022-04-20 23:23:04 +0100

    Added form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,15 @@ def index():
 @app.route('/downloader')
 def downloader():
     return render_template('downloader.html')
+
+@app.route('/data', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/downloader' to submit form"
+    if request.method == 'POST':
+        f"POST Result"
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
