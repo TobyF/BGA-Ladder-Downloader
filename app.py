63535e1c006547dfbc05f8df5870fa59dabc933d
Toby Freeland <tobyfreeland@gmail.com>
2022-04-20 20:57:13 +0100

    fixed static

--- app.py
+++ app.py
@@ -2,5 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return return render_template('index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/downloader')
+def downloader():
+    return render_template('downloader.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
