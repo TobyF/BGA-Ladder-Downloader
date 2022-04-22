75e52d0866e44e1fb34b3b619c0a1e5d56c3eb97
Toby Freeland <tobyfreeland@gmail.com>
2022-04-22 19:26:48 +0100

    Basic pilotID->flightZIP

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, send_file
+from bga_ladder_tools import get_flight_ids, get_pilot_id, get_and_zip_igcs
+import tempfile
+import zipfile
+import io
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,9 +18,21 @@ def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/downloader' to submit form"
     if request.method == 'POST':
-        f"POST Result"
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+
+        pilot_id = request.form['pilot_id']
+        flight_ids = get_flight_ids(pilot_id)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            print('created temporary directory', tmpdir)
+            flights_zip = get_and_zip_igcs(flight_ids,tmpdir)
+        #flights_zip_loc = 'test_data/flights.zip'
+        #print(flights_zip_loc)
+        #with open(flights_zip_loc,'r') as f:
+        #    content = f.read()
+        return send_file(flights_zip,as_attachment=True, download_name='flights.zip')
+
+@app.route('/pilotdata')
+def pilotdata():
+    return send_from_directory('static','active_pilots.txt',as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
---
+++ bga_ladder_tools.py
@@ -0,0 +1,77 @@
+import json
+import requests
+from urllib.request import urlopen
+import ssl
+import re
+import shutil
+import os
+import zipfile
+import io
+
+def make_archive(source, destination):
+        base = os.path.basename(destination)
+        name = base.split('.')[0]
+        format = base.split('.')[1]
+        archive_from = os.path.dirname(source)
+        archive_to = os.path.basename(source.strip(os.sep))
+        shutil.make_archive(name, format, archive_from, archive_to)
+        shutil.move('%s.%s'%(name,format), destination)
+
+def get_pilot_id(name):
+    pass
+
+def get_flight_ids(pilot_id,start_year=2022,end_year=2022):
+    flightIDs = []
+    for year in range(start_year,end_year+1):
+        query_url = f"https://bgaladder.net/API/DailyScores?Season={year}&Pilot={pilot_id}"
+        page = requests.get(query_url)
+        page_json = json.loads(page.content)
+        for flight in page_json["rows"]:
+            flightIDs.append(flight["FlightID"])
+    return flightIDs
+
+def download_igc_file(flight_id, dir):
+    url = f'http://bgaladder.net/FlightIGC/{flight_id}'
+    r = requests.get(url, allow_redirects=True)
+    try:
+        d = r.headers['content-disposition']
+        fname = ((re.findall("filename=(.+)", d)[0]).split(';')[0]).strip('"') #extracts filename, likely dodgy.
+    except KeyError:
+        fname=f"{flight_id}.igc"
+    print(fname)
+    print(dir)
+    print(os.path.join(dir,fname))
+    with open(os.path.join(dir,fname), 'wb') as f:
+        f.write(r.content)
+
+def get_and_zip_igcs(flight_ids,tmpdir):
+    base_path = os.path.join(tmpdir,'flights')
+    os.makedirs(base_path)
+    print(base_path)
+    for flight_id in flight_ids:
+        download_igc_file(flight_id, base_path)
+    #make_archive(os.path.join(tmpdir,'flights'), os.path.join(tmpdir,'ladder_flights.zip'))
+
+    data = io.BytesIO()
+    with zipfile.ZipFile(data, mode='w') as z:
+        for f_name in os.listdir(path=base_path):
+            z.write(os.path.join(base_path,f_name),arcname = f_name)
+    data.seek(0)
+    #zip_loc = shutil.make_archive(os.path.join(tmpdir,'flights'),'zip',os.path.join(tmpdir,'flights'))
+    return data
+
+if __name__ == '__main__':
+    print("Testing...")
+    #print('Flight IDs :')
+    #print(get_flight_ids(3851))
+    print('Downloading IGC file:')
+    #download_igc_file(66505, 'test_data')
+
+
+    dir = os.path.join(os.getcwd(),'test_data')
+    print(f"Making a test directory: {dir}")
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
+
+    print(get_and_zip_igcs(get_flight_ids(3851),dir))
